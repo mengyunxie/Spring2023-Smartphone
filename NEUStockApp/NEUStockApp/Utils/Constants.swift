bf93c3be31b18330f6357e775a62db37b6ed2fbd
mengyunxie <my.xiemy@gmail.com>
2023-02-14 11:20:14 -0800

    Install pods

---
+++ NEUStockApp/NEUStockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  NEUStockApp
+//
+//  Created by Miya on 2/11/23.
+//
+
+import Foundation
+
+let apiKey = "4cb6f8cc1d8aae00e0da916cdf622ab9"
+let baseURL = "https://financialmodelingprep.com/api/v3/quote-short/"
---
+++ NEUStockApp/NEUStockApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NEUStockApp
+//
+//  Created by Miya on 2/11/23.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import SwiftSpinner
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
