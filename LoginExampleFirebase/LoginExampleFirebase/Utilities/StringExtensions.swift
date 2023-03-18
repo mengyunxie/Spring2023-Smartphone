3055e4d1a68e162193b3a59bcc4edf7430e91cf0
mengyunxie <my.xiemy@gmail.com>
2023-03-18 12:31:25 -0700

    Learning login with firebase

---
+++ LoginExampleFirebase/LoginExampleFirebase/Utilities/StringExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  StringExtensions.swift
+//  LoginExampleFirebase
+//
+//  Created by Miya on 3/18/23.
+//
+
+import Foundation
+
+extension String {
+    var isEmail: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+    
+    var isValidPassword : Bool {
+        let passwordRegEx = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&]).{6,}$")
+        return passwordRegEx.evaluate(with: self)
+    }
+}
+
+
+
+
+
--- LoginExampleFirebase/LoginExampleFirebase/ViewController.swift
+++ LoginExampleFirebase/LoginExampleFirebase/ViewController.swift
@@ -18,11 +18,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         lblStatus.isHidden = true
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let keyChain = KeychainService().keyChain
+        if keyChain.get("uid") != nil {
+            performSegue(withIdentifier: "segueDashboard", sender: self)
+        }
+        txtPassword.text = ""
+    }
+    
+    func addKeyChainAfterLogin(uid : String){
+        let keyChain = KeychainService().keyChain
+        keyChain.set(uid, forKey: "uid")
+    }
 
     @IBAction func loginAction(_ sender: Any) {
         
         guard let email = txtEmail.text else {return}
         guard let password = txtPassword.text else {return}
+        
+        if email.isEmail == false {
+            lblStatus.text = "Enter valid email"
+            lblStatus.isHidden = false
+            return
+        }
+        
+        if password.isValidPassword == false {
+            lblStatus.text = "Enter valid password"
+            lblStatus.isHidden = false
+            return
+        }
         
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
             guard let strongSelf = self else { return }
@@ -32,6 +57,12 @@ class ViewController: UIViewController {
                 strongSelf.lblStatus.isHidden = false
                 return
             }
+            strongSelf.lblStatus.text = "Login Successful"
+            strongSelf.lblStatus.isHidden = true
+            
+            guard let uid = Auth.auth().currentUser?.uid else {return}
+            strongSelf.addKeyChainAfterLogin(uid:uid)
+            
             strongSelf.performSegue(withIdentifier: "segueDashboard", sender: strongSelf)
         }
     }
