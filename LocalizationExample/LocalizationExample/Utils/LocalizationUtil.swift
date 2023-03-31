e8c492d3607ed13643e54397143a1186b85ff2f4
mengyunxie <my.xiemy@gmail.com>
2023-03-30 21:36:10 -0700

    Learning localization

---
+++ LocalizationExample/LocalizationExample/Utils/LocalizationUtil.swift
@@ -0,0 +1,10 @@
+//
+//  LocalizationUtil.swift
+//  LocalizationExample
+//
+//  Created by Miya on 3/30/23.
+//
+
+import Foundation
+
+var helloWorld = NSLocalizedString("hello_world", comment: "This is hello world string")
